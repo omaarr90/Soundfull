1a4df379f11cbe31e56d5988493846ecea5294b8
Omar Alshammary <omar.alshammary@hotmail.com>
2016-06-25 04:25:25 +0300

    Use correct file path

---
+++ Soundfull/MusicDownloader.swift
@@ -0,0 +1,13 @@
+//
+//  MusicDownloader.swift
+//  Soundfull
+//
+//  Created by Omar Alshammari on 6/25/16.
+//  Copyright © 2016 Omar Alshammari. All rights reserved.
+//
+
+import UIKit
+
+class MusicDownloader: NSObject {
+
+}
---
+++ Soundfull/SoundConverter.swift
@@ -0,0 +1,13 @@
+//
+//  SoundConverter.swift
+//  Soundfull
+//
+//  Created by Omar Alshammari on 6/25/16.
+//  Copyright © 2016 Omar Alshammari. All rights reserved.
+//
+
+import UIKit
+
+class SoundConverter: NSObject {
+
+}
